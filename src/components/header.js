12322526f1a58a95860e8abb833cdef264f52151
shravya012 <Sudireddy.shravya@jktech.com>
2024-06-17 13:55:58 +0530

    Completed Exercise-1: 3.1 and 3.2

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../styles/Header.css';
 
 const Header = () => {
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+
+    const handleThemeChange = (event) => {
+        setTheme(event.target.value);
+    };
+
+
     return (
         <header className="app-header">
             <h1 className="app-title">Users List</h1>
             <div className="theme-toggle">
                 <label>
-                    <input type="radio" name="theme" value="light" />
+                    <input
+                        type="radio"
+                        name="theme" 
+                        value="light"
+                        checked={theme === "light"}
+                        onChange={handleThemeChange} 
+                    />
                     Light
                 </label>
                 <label>
-                    <input type="radio" name="theme" value="dark" />
+                    <input 
+                        type="radio" 
+                        name="theme" 
+                        value="dark" 
+                        checked={theme === "dark"}
+                        onChange={handleThemeChange}
+                    />
                     Dark
                 </label>
             </div>
@@ -20,3 +44,5 @@ const Header = () => {
 };
 
 export default Header;
+ 
+
