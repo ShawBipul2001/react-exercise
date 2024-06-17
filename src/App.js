b2a582f4225b886e051e7e46485e00f004496d4f
Jatin <jatin.motwani@jktech.com>
2024-06-17 11:31:35 +0530

    added content, usertable, and rolechange components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from './components/header';
+import Content from './components/Content';
 
 function App() {
   return (
     <>
       <Header />
-      {/* Create more components here */}
+      <Content />
     </>
   );
 }
